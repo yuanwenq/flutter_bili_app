c97e767a699a87d648c370bf1af93ca7b4a28c71
干将 <hyw232@163.com>
2022-10-05 20:48:25 +0800

    update: 1.视频详情页相关实现

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -5,9 +5,12 @@ import 'package:flutter_bili_app/http/dao/home_dao.dart';
 import 'package:flutter_bili_app/model/home_model.dart';
 import 'package:flutter_bili_app/navigator/hi_navigator.dart';
 import 'package:flutter_bili_app/page/home_tab_page.dart';
+import 'package:flutter_bili_app/page/profile.dart';
+import 'package:flutter_bili_app/page/video_detail_page.dart';
 import 'package:flutter_bili_app/util/color.dart';
 import 'package:flutter_bili_app/util/toast_util.dart';
 import 'package:flutter_bili_app/util/view_util.dart';
+import 'package:flutter_bili_app/widget/hi_tab.dart';
 import 'package:flutter_bili_app/widget/loading_container.dart';
 import 'package:flutter_bili_app/widget/navigation_bar.dart';
 
@@ -22,19 +25,26 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends HiState<HomePage>
-    with AutomaticKeepAliveClientMixin, TickerProviderStateMixin {
+    with
+        AutomaticKeepAliveClientMixin,
+        TickerProviderStateMixin,
+        WidgetsBindingObserver {
   var listener;
   late TabController _controller;
   // var tabs = ["推荐", "热门", "追播", "影视", "搞笑", "日常", "综合", "手机游戏", "短片·手书·配音"];
   List<CategoryModel> categoryList = [];
   List<BannerModel> bannerList = [];
   bool _isLoading = true;
+  late Widget _currentPage;
 
   @override
   void initState() {
     super.initState();
+    WidgetsBinding.instance.addObserver(this);
+
     _controller = TabController(length: categoryList.length, vsync: this);
     HiNavigator.getInstance().addListener(listener = (current, pre) {
+      this._currentPage = current.page;
       print("home: current:${current.page}");
       print("home: pre:${pre.page}");
       if (widget == current.page || current.page is HomePage) {
@@ -42,15 +52,44 @@ class _HomePageState extends HiState<HomePage>
       } else if (widget == pre?.page || pre?.page is HomePage) {
         print("首页: onPause");
       }
+      // 当页面返回到首页恢复首页的状态栏样式
+      if (pre?.page is VideoDetailPage && current.page is! ProfilePage) {
+        var statusStyle = StatusStyle.DARK_CONTENT;
+        changeStatusBar(color: Colors.white, statusStyle: statusStyle);
+      }
     });
     loadData();
   }
 
   @override
   void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+
     HiNavigator.getInstance().removeListener(listener);
     _controller.dispose();
     super.dispose();
+  }
+
+  /// 监听应用生命周期变化
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    print(":didChangeAppLifecycleState:$state");
+    switch (state) {
+      case AppLifecycleState.inactive: // 处于这种状态的应用程序应该假设它们可能在任何时候暂停
+        break;
+      case AppLifecycleState.resumed: // 从后台切换前台，界面可见
+        // fix Android 压后台，状态栏字体颜色变白问题
+        if (_currentPage is! VideoDetailPage) {
+          changeStatusBar(
+              color: Colors.white, statusStyle: StatusStyle.DARK_CONTENT);
+        }
+        break;
+      case AppLifecycleState.paused: // 界面不可见，后台状态
+        break;
+      case AppLifecycleState.detached: // APP结束时调用
+        break;
+    }
   }
 
   @override
@@ -90,25 +129,18 @@ class _HomePageState extends HiState<HomePage>
   bool get wantKeepAlive => true;
 
   _tabBar() {
-    return TabBar(
-        controller: _controller,
-        isScrollable: true,
-        labelColor: Colors.black,
-        indicator: UnderlineIndicator(
-            strokeCap: StrokeCap.round,
-            borderSide: BorderSide(color: primary, width: 3),
-            insets: EdgeInsets.only(left: 15, right: 15)),
-        tabs: categoryList.map<Tab>((tab) {
-          return Tab(
-            child: Padding(
-              padding: EdgeInsets.only(left: 5, right: 5),
-              child: Text(
-                tab.name,
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
-          );
-        }).toList());
+    return HiTab(
+      categoryList.map<Tab>((tab) {
+        return Tab(
+          text: tab.name,
+        );
+      }).toList(),
+      controller: _controller,
+      fontSize: 16,
+      borderWidth: 3,
+      unselectedLabelColor: Colors.black54,
+      insets: 13,
+    );
   }
 
   void loadData() async {
--- lib/page/video_detail_page.dart
+++ lib/page/video_detail_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bili_app/model/video_model.dart';
 import 'package:flutter_bili_app/util/view_util.dart';
 import 'package:flutter_bili_app/widget/appbar_widget.dart';
+import 'package:flutter_bili_app/widget/hi_tab.dart';
 import 'package:flutter_bili_app/widget/navigation_bar.dart';
 import 'package:flutter_bili_app/widget/video_view.dart';
 
@@ -15,7 +16,23 @@ class VideoDetailPage extends StatefulWidget {
   State<VideoDetailPage> createState() => _VideoDetailPageState();
 }
 
-class _VideoDetailPageState extends State<VideoDetailPage> {
+class _VideoDetailPageState extends State<VideoDetailPage>
+    with TickerProviderStateMixin {
+  late TabController _controller;
+  List tabs = ["简介", "评论2888"];
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TabController(length: tabs.length, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,20 +46,57 @@ class _VideoDetailPageState extends State<VideoDetailPage> {
                 statusStyle: StatusStyle.LIGHT_CONTENT,
                 height: Platform.isAndroid ? 0 : 46,
               ),
-              _videoView(),
-              Text("视频详情页，vid:${widget.videoModel.vid}"),
-              Text("视频详情页，title:${widget.videoModel.title}"),
+              _buildVideoView(),
+              _buildTabNavigation(),
             ],
           )),
     );
   }
 
-  _videoView() {
+  _buildVideoView() {
     var model = widget.videoModel;
     return VideoView(
       model.url!,
       cover: model.cover,
       overlayUi: videoAppBar(),
+    );
+  }
+
+  _buildTabNavigation() {
+    // 使用 Material 实现阴影效果
+    return Material(
+      elevation: 5,
+      shadowColor: Colors.grey[100],
+      child: Container(
+        alignment: Alignment.centerLeft,
+        padding: const EdgeInsets.only(left: 20, right: 20),
+        height: 39,
+        color: Colors.white,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            _tabBar(),
+            Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Icon(
+                Icons.live_tv_rounded,
+                color: Colors.grey,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  _tabBar() {
+    return HiTab(
+      tabs.map<Tab>((name) {
+        return Tab(
+          text: name,
+        );
+      }).toList(),
+      controller: _controller,
     );
   }
 }
